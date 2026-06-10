b5e2160275ce5e8d8a898ef460b4bba21a77b105
marianmatus73-dot <marianmatus73@gmail.com>
2026-06-10 12:52:41 +0200

    Add history_sync.py for data export mappings

---
+++ core/history_sync.py
@@ -0,0 +1,9 @@
+pred scanom:
+exports/history_sport_bets.csv -> sport_bets
+exports/history_bookmaker_stats.csv -> sport_bookmaker_stats
+exports/history_elo_ratings.csv -> sport_elo_ratings
+
+po scane:
+sport_bets -> exports/history_sport_bets.csv
+sport_bookmaker_stats -> exports/history_bookmaker_stats.csv
+sport_elo_ratings -> exports/history_elo_ratings.csv
