1c0bc411658249c221dcd3bf74baf58396188304
marianmatus73-dot <marianmatus73@gmail.com>
2026-07-17 17:59:32 +0200

    Add files via upload

---
+++ main_v15_real_clv_pipeline_v1553.py
@@ -0,0 +1,13 @@
+from core.football_real_closing_odds_merge_v15_53 import (
+    run_real_closing_odds_merge_v15_53,
+)
+
+
+def run_full_pipeline_v15_53():
+    return {
+        "real_closing_merge": "READY"
+    }
+
+
+if __name__ == "__main__":
+    print(run_full_pipeline_v15_53())
